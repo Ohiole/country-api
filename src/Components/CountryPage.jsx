7078a9600107374589b2baa9e8e4d18d33406f97
Ohiole <eboreimeojo99@gmail.com>
2023-10-06 23:54:20 +0100

    New commit

---
+++ src/Components/CountryPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CountryPage() {
+  return (
+    <section className='countryInfo'>
+        
+    </section>
+  )
+}
+
+export default CountryPage
--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faArrowLeft, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Data } from '../data';
 import CountryItem from './CountryItem';
+import CountryPage from './CountryPage';
 
 function MainPage() {
     const [selectValue, setSelectValue] = useState('Filter by region');
@@ -31,32 +32,53 @@ function MainPage() {
     let europe = Data.filter(item => item.region === 'Europe');
     let oceania = Data.filter(item => item.region === 'Oceania');
 
+    function searchIt(e) {
+        setSearchValue(e.target.value)
+    }
+
   return (
     <main>
-        <section className="search">
-            <section className="input">
-                <FontAwesomeIcon icon={faSearch} />
-                <input type="text" placeholder='Search for a country...' value={searchValue} onChange={() => setSearchValue()} />
-            </section>
-            <section className="options">
-                <section className="filter" onClick={() => setIsOpen(!isOpen)}>
-                    <p>{selectValue}</p>
-                    <FontAwesomeIcon icon={faAngleDown} className='icon' />
+        <section className='countryPage'>
+            <section className="search">
+                <section className="input">
+                    <FontAwesomeIcon icon={faSearch} />
+                    <input type="text" placeholder='Search for a country...' value={searchValue} onChange={searchIt} />
                 </section>
-                { isOpen ? <section className="eachOpt">
-                    <p onClick={() => updateValue('Filter by region')}>All</p>
-                    <p onClick={() => updateValue('Africa')}>Africa</p>
-                    <p onClick={() => updateValue('America')}>America</p>
-                    <p onClick={() => updateValue('Asia')}>Asia</p>
-                    <p onClick={() => updateValue('Europe')}>Europe</p>
-                    <p onClick={() => updateValue('Oceania')}>Oceania</p>
-                </section> : ''}
+                <section className="options">
+                    <section className="filter" onClick={() => setIsOpen(!isOpen)}>
+                        <p>{selectValue}</p>
+                        <FontAwesomeIcon icon={faAngleDown} className='icon' />
+                    </section>
+                    { isOpen ? <section className="eachOpt">
+                        <p onClick={() => updateValue('Filter by region')}>All</p>
+                        <p onClick={() => updateValue('Africa')}>Africa</p>
+                        <p onClick={() => updateValue('America')}>America</p>
+                        <p onClick={() => updateValue('Asia')}>Asia</p>
+                        <p onClick={() => updateValue('Europe')}>Europe</p>
+                        <p onClick={() => updateValue('Oceania')}>Oceania</p>
+                    </section> : ''}
+                </section>
             </section>
-        </section>
-        <section className="countries">
-            { 
-                selectValue === 'Filter by region' &&
-                (Data.map((item, key) => 
+            <section className="countries">
+                { 
+                    selectValue === 'Filter by region' &&
+                    (Data.filter((item) => {
+                        return searchValue.toLowerCase() ===  '' ? item : item.name.toLowerCase().includes(searchValue)
+                    }).map((item, key) => 
+                        <CountryItem 
+                            key={key}
+                            image={item.flags.png}
+                            name={item.name}
+                            population={item.population}
+                            region={item.region}
+                            capital={item.capital}
+                        />))
+                } 
+                {
+                    selectValue === 'Africa' &&
+                    (africa.filter((item) => {
+                        return searchValue.toLowerCase() ===  '' ? item : item.name.toLowerCase().includes(searchValue)
+                    }).map((item, key) => 
                     <CountryItem 
                         key={key}
                         image={item.flags.png}
@@ -64,68 +86,72 @@ function MainPage() {
                         population={item.population}
                         region={item.region}
                         capital={item.capital}
-                    />))
-            } 
-            {
-                selectValue === 'Africa' &&
-                (africa.map((item, key) => 
-                <CountryItem 
-                    key={key}
-                    image={item.flags.png}
-                    name={item.name}
-                    population={item.population}
-                    region={item.region}
-                    capital={item.capital}
-                /> ))
-            }
-            {
-                selectValue === 'America' &&
-                (america.map((item, key) => 
-                <CountryItem 
-                    key={key}
-                    image={item.flags.png}
-                    name={item.name}
-                    population={item.population}
-                    region={item.region}
-                    capital={item.capital}
-                /> ))
-            }
-            {
-                selectValue === 'Asia' &&
-                (asia.map((item, key) => 
-                <CountryItem 
-                    key={key}
-                    image={item.flags.png}
-                    name={item.name}
-                    population={item.population}
-                    region={item.region}
-                    capital={item.capital}
-                /> ))
-            }
-            {
-                selectValue === 'Europe' &&
-                (europe.map((item, key) => 
-                <CountryItem 
-                    key={key}
-                    image={item.flags.png}
-                    name={item.name}
-                    population={item.population}
-                    region={item.region}
-                    capital={item.capital}
-                /> ))
-            }
-            {
-                selectValue === 'Oceania' &&
-                (oceania.map((item, key) => 
-                <CountryItem 
-                    key={key}
-                    image={item.flags.png}
-                    name={item.name}
-                    population={item.population}
-                    region={item.region}
-                    capital={item.capital}
-                /> ))
-            }
+                    /> ))
+                }
+                {
+                    selectValue === 'America' &&
+                    (america.filter((item) => {
+                        return searchValue.toLowerCase() ===  '' ? item : item.name.toLowerCase().includes(searchValue)
+                    }).map((item, key) => 
+                    <CountryItem 
+                        key={key}
+                        image={item.flags.png}
+                        name={item.name}
+                        population={item.population}
+                        region={item.region}
+                        capital={item.capital}
+                    /> ))
+                }
+                {
+                    selectValue === 'Asia' &&
+                    (asia.filter((item) => {
+                        return searchValue.toLowerCase() ===  '' ? item : item.name.toLowerCase().includes(searchValue)
+                    }).map((item, key) => 
+                    <CountryItem 
+                        key={key}
+                        image={item.flags.png}
+                        name={item.name}
+                        population={item.population}
+                        region={item.region}
+                        capital={item.capital}
+                    /> ))
+                }
+                {
+                    selectValue === 'Europe' &&
+                    (europe.filter((item) => {
+                        return searchValue.toLowerCase() ===  '' ? item : item.name.toLowerCase().includes(searchValue)
+                    }).map((item, key) => 
+                    <CountryItem 
+                        key={key}
+                        image={item.flags.png}
+                        name={item.name}
+                        population={item.population}
+                        region={item.region}
+                        capital={item.capital}
+                    /> ))
+                }
+                {
+                    selectValue === 'Oceania' &&
+                    (oceania.filter((item) => {
+                        return searchValue.toLowerCase() ===  '' ? item : item.name.toLowerCase().includes(searchValue)
+                    }).map((item, key) => 
+                    <CountryItem 
+                        key={key}
+                        image={item.flags.png}
+                        name={item.name}
+                        population={item.population}
+                        region={item.region}
+                        capital={item.capital}
+                    /> ))
+                }
+            </section>
+        </section>
+        <section className="clickCountry">
+            <section className="button">
+                <FontAwesomeIcon icon={faArrowLeft} />
+                <button>Back</button>
+            </section>
+            <CountryPage />
         </section>
     </main>
   )
